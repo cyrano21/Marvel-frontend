4d6c762a11a838807f85271787e827a6eb60ed15
Nkeng hiag louis olivier <louiscyrano@gmail.com>
2023-10-06 00:43:58 +0200

    add background home gif

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,165 @@
+// import "../Home/home.css";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import Carousel from "react-elastic-carousel";
+// import { useNavigate } from "react-router-dom";
+
+// export default function Home() {
+//   const [characters, setCharacters] = useState([]);
+//   const [comics, setComics] = useState([]);
+//   const [itemsToShow, setItemsToShow] = useState(5); // nouvel état pour le nombre d'items à afficher
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const fetchCharacters = async () => {
+//       const response = await axios.get(
+//         "https://site--marvel-backend--cl5kfjmsrksj.code.run/characters"
+//       );
+//       setCharacters(response.data.results);
+//     };
+
+//     const fetchComics = async () => {
+//       const response = await axios.get(
+//         "https://site--marvel-backend--cl5kfjmsrksj.code.run/comics"
+//       );
+//       setComics(response.data.results);
+//     };
+
+//     fetchCharacters();
+//     fetchComics();
+
+//     // Nouvelle fonction pour mettre à jour le nombre d'items en fonction de la largeur de l'écran
+//     const mettreAJourItemsToShow = () => {
+//       if (window.innerWidth <= 480) {
+//         setItemsToShow(1);
+//       } else if (window.innerWidth <= 900) {
+//         setItemsToShow(3);
+//       } else if (window.innerWidth <= 1024) {
+//         setItemsToShow(4);
+//       } else {
+//         setItemsToShow(5);
+//       }
+//     };
+
+//     // Vérification initiale
+//     mettreAJourItemsToShow();
+
+//     // Écoute des événements de redimensionnement de la fenêtre
+//     window.addEventListener("resize", mettreAJourItemsToShow);
+
+//     // Supprimez l'écouteur lorsque le composant est démonté
+//     return () => window.removeEventListener("resize", mettreAJourItemsToShow);
+//   }, []);
+
+//   return (
+//     <main>
+//       <div className="welcome-text">
+//         <h1>
+//           <p className="welcome">WELCOME TO MARVEL</p>
+//           <p className="home">HOME !</p>
+//         </h1>
+//       </div>
+
+//       <div className="welcome-title">
+//         <div className="part-char">
+//           <h1
+//             className="characters "
+//             onClick={() => {
+//               navigate("/characters");
+//             }}
+//           >
+//             PERSONNAGES
+//           </h1>
+//           <Carousel
+//             itemsToShow={itemsToShow}
+//             enableAutoPlay
+//             autoPlaySpeed={5000}
+//           >
+//             {characters.map((character) => (
+//               <div
+//                 key={character._id}
+//                 className="home-char"
+//                 onClick={() => navigate(`/character/${character._id}`)}
+//               >
+//                 <div className="p-title">
+//                   <p>{character.name}</p>
+//                 </div>
+
+//                 <div className="img-title">
+//                   <img
+//                     src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+//                     alt={character.name}
+//                     style={{ border: "2px solid #000" }}
+//                     className="responsive-image"
+//                   />
+
+//                   <div className="img-title-text">
+//                     {character.description && (
+//                       <span className="description-title">Description:</span>
+//                     )}
+//                     <p className="description">{character.description}</p>
+//                   </div>
+//                 </div>
+//               </div>
+//             ))}
+//           </Carousel>
+//         </div>
+
+//         <div className="part-comic">
+//           <h1
+//             className="comics"
+//             onClick={() => {
+//               navigate("/comics");
+//             }}
+//           >
+//             COMICS
+//           </h1>
+//           <Carousel
+//             itemsToShow={3}
+//             itemPadding={[0, 10, 0, 10]}
+//             enableAutoPlay
+//             autoPlaySpeed={10000}
+//           >
+//             {comics.map((comic) => (
+//               <div
+//                 key={comic._id}
+//                 className="home-comic"
+//                 onClick={() => navigate(`/comic/${comic._id}`)}
+//               >
+//                 <div className="p-title">
+//                   <p>{comic.title}</p>
+//                 </div>
+
+//                 <img
+//                   src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+//                   alt={comic.title}
+//                   style={{ border: "2px solid #000" }}
+//                   className="responsive-image"
+//                 />
+//               </div>
+//             ))}
+//           </Carousel>
+//         </div>
+//       </div>
+//       <button className="home-button">Home</button>
+//     </main>
+//   );
+// }
+
+// import "../Home/home.css";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import Carousel from "react-elastic-carousel";
+// import { useNavigate } from "react-router-dom";
+
+// export default function Home() {
+//   const [characters, setCharacters] = useState([]);
+//   const [comics, setComics] = useState([]);
+//   const [itemsToShow, setItemsToShow] = useState(5);
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+
 import "../Home/home.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -7,9 +169,24 @@ import { useNavigate } from "react-router-dom";
 export default function Home() {
   const [characters, setCharacters] = useState([]);
   const [comics, setComics] = useState([]);
-  const [itemsToShow, setItemsToShow] = useState(5); // nouvel état pour le nombre d'items à afficher
+  const [itemsToShow, setItemsToShow] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1); // Nouvel état pour la pagination
   const navigate = useNavigate();
 
+  const getAnimationClass = (index) => {
+    switch (index % 4) {
+      case 0:
+        return "slideInFromLeft";
+      case 1:
+        return "slideInFromRight";
+      case 2:
+        return "slideInFromTop";
+      case 3:
+        return "slideInFromBottom";
+      default:
+        return "";
+    }
+  };
   useEffect(() => {
     const fetchCharacters = async () => {
       const response = await axios.get(
@@ -28,7 +205,6 @@ export default function Home() {
     fetchCharacters();
     fetchComics();
 
-    // Nouvelle fonction pour mettre à jour le nombre d'items en fonction de la largeur de l'écran
     const mettreAJourItemsToShow = () => {
       if (window.innerWidth <= 480) {
         setItemsToShow(1);
@@ -41,15 +217,20 @@ export default function Home() {
       }
     };
 
-    // Vérification initiale
     mettreAJourItemsToShow();
 
-    // Écoute des événements de redimensionnement de la fenêtre
-    window.addEventListener("resize", mettreAJourItemsToShow);
+    //   window.addEventListener("resize", mettreAJourItemsToShow);
 
-    // Supprimez l'écouteur lorsque le composant est démonté
+    //   return () => window.removeEventListener("resize", mettreAJourItemsToShow);
+    // }, []);
+
+    window.addEventListener("resize", mettreAJourItemsToShow);
     return () => window.removeEventListener("resize", mettreAJourItemsToShow);
   }, []);
+
+  const handlePageChange = (currentItem, pageIndex) => {
+    setCurrentPage(pageIndex + 1);
+  };
 
   return (
     <main>
@@ -68,17 +249,20 @@ export default function Home() {
               navigate("/characters");
             }}
           >
+            {" "}
             PERSONNAGES
           </h1>
           <Carousel
+            className="my-carousel"
             itemsToShow={itemsToShow}
             enableAutoPlay
             autoPlaySpeed={5000}
+            paginationType="numbers"
           >
-            {characters.map((character) => (
+            {characters.map((character, index) => (
               <div
                 key={character._id}
-                className="home-char"
+                className={`home-char ${getAnimationClass(index)}`}
                 onClick={() => navigate(`/character/${character._id}`)}
               >
                 <div className="p-title">
@@ -115,10 +299,10 @@ export default function Home() {
             COMICS
           </h1>
           <Carousel
-            itemsToShow={3}
+            itemsToShow={1}
             itemPadding={[0, 10, 0, 10]}
             enableAutoPlay
-            autoPlaySpeed={10000}
+            autoPlaySpeed={5000}
           >
             {comics.map((comic) => (
               <div
